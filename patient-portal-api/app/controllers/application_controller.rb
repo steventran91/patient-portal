4779ad1780bae7542524c576c1e3b01d64e6dfcc
Steven Tran <sdtran91@gmail.com>
2026-07-01 19:28:59 -0700

    complete authorization controller

--- patient-portal-api/app/controllers/application_controller.rb
+++ patient-portal-api/app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+    before_action :authenticate_request
+
+    private
+    def authenticate_request
+        token = request.headers["Authorization"]&.split(" ")&.last 
+        decoded = JwtService.decode(token)
+        @current_user = User.find_by(id: decoded&.[]("user_id"))
+    end
 end
